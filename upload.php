ae68065c19d04650d83446fc969642327cefef68
ShreyasDol <37362810+ShreyasDol@users.noreply.github.com>
2019-11-15 08:42:33 +0530

    Update upload.php
    Please create "image" folder on your local directory.

--- upload.php
+++ upload.php
@@ -1,5 +1,9 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 if(isset($_FILES['file_upload'])){
       $errors= array();
@@ -7,19 +11,17 @@ if(isset($_FILES['file_upload'])){
       $file_size =$_FILES['file_upload']['size'];
       $file_tmp =$_FILES['file_upload']['tmp_name'];
       $file_type=$_FILES['file_upload']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['file_upload']['name'])));
-      $extensions= array("jpeg","jpg","png");
      
       $target = "images/".$file_name;
       if(empty($errors)==true){
-			move_uploaded_file($file_tmp,"images/".$file_name);
-			chmod("images/".$file_name, 0777);
-			$filename = "http://localhost/filetohex/images/".$file_name;
-			$hex = unpack("H*", file_get_contents($filename));
-			$hex = current($hex);
-			$return = array('code'=>200,'hex'=>$hex);
+         move_uploaded_file($file_tmp,"images/".$file_name);
+         chmod("images/".$file_name, 0777);
+         $filename = "http://localhost/filetohex/images/".$file_name;
+         $hex = unpack("H*", file_get_contents($filename));
+         $hex = current($hex);
+         $return = array('code'=>200,'hex'=>$hex);
       }else{
-			$return = array('code'=>100);
+         $return = array('code'=>100);
       }
 
       echo json_encode($return);
